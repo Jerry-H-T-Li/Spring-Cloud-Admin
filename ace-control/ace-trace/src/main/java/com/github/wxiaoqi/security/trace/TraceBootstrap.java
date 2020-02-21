406b4b301a34929679c3de8b98d6ba4a66179473
Lht <381846741@qq.com>
2020-02-21 21:43:54 +0800

    test

--- ace-control/ace-trace/src/main/java/com/github/wxiaoqi/security/trace/TraceBootstrap.java
+++ ace-control/ace-trace/src/main/java/com/github/wxiaoqi/security/trace/TraceBootstrap.java
@@ -27,13 +27,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.EnableZipkinServer;
 
+
 /**
  * Created by ace on 2017/7/10.
  */
 @SpringBootApplication
 @EnableZipkinServer
 public class TraceBootstrap {
+
     public static void main(String[] args) {
-        SpringApplication.run(TraceBootstrap.class,args);
+        SpringApplication.run(TraceBootstrap.class, args);
     }
 }
